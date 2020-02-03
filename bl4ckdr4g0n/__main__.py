d707f1c51ab4d0c7f188421efeea64b85a747813
dr4g0n18 <59910652+dr4g0n18@users.noreply.github.com>
2020-02-04 00:13:29 +0800

    Update __main__.py

--- bl4ckdr4g0n/__main__.py
+++ bl4ckdr4g0n/__main__.py
@@ -662,4 +662,18 @@ if __name__ == '__main__':
     LOGGER.info("Successfully loaded")
     main()
 
-    #this bot made by @virtualjust0and1 Linux404
+#𝕭𝖑𝖆𝖈𝖐𝕯𝖗𝖆𝖌𝖔𝖓 bot
+a modular telegram Python bot running on python3 with an sqlalchemy database.
+
+Originally a simple group management bot with multiple admin features, it has evolved, becoming extremely modular and 
+simple to use".
+
+Can be found on telegram as [𝕭𝖑𝖆𝖈𝖐𝕯𝖗𝖆𝖌𝖔𝖓](https://t.me/dr4gonbot).
+
+We've a [learning group](https://t.me/joinchat/EnD9SkO9Awougo5VjknbrQ),
+
+Join the [learning channel](https://t.me/EnglishFreeLectures) For Free Knowledge.
+
+Alternatively, [find me on telegram](https://t.me/virtualjust0and1)! (Keep all support questions in the support chat, where more people can help you.)
+
+This Bot Is Made By [@virtualjust0and1](https://t.me/virtualjust0and1)
