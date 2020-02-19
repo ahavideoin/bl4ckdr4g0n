7bf5da5c29dce82d8652648f7892e4e9fe98b71c
Threemtg <61043377+Threemtg@users.noreply.github.com>
2020-02-19 11:47:57 -0800

    Update __main__.py

--- bl4ckdr4g0n/__main__.py
+++ bl4ckdr4g0n/__main__.py
@@ -10,8 +10,7 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryH
 from telegram.ext.dispatcher import run_async, DispatcherHandlerStop, Dispatcher
 from telegram.utils.helpers import escape_markdown
 
-from bl4ckdr4g0n import dispatcher, updater, TOKEN, WEBHOOK, SUDO_USERS, CERT_PATH, PORT, URL, LOGGER, \
-    ALLOW_EXCL
+import bl4ckdr4g0n
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from bl4ckdr4g0n.modules import ALL_MODULES
