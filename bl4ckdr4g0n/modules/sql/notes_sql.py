e16776f53bf5550e8cac5f0dda026ab1a54e4b41
Threemtg <61043377+Threemtg@users.noreply.github.com>
2020-02-29 17:02:16 -0800

    Add files via upload

--- bl4ckdr4g0n/modules/sql/notes_sql.py
+++ bl4ckdr4g0n/modules/sql/notes_sql.py
@@ -1,11 +1,10 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
-from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from bl4ckdr4g0n.modules.helper_funcs.msg_types import Types
-from bl4ckdr4g0n.modules.sql import SESSION, BASE
+from tg_bot.modules.helper_funcs.msg_types import Types
+from tg_bot.modules.sql import SESSION, BASE
 
 
 class Notes(BASE):
@@ -45,11 +44,13 @@ class Buttons(BASE):
         self.url = url
         self.same_line = same_line
 
+
 Notes.__table__.create(checkfirst=True)
 Buttons.__table__.create(checkfirst=True)
 
 NOTES_INSERTION_LOCK = threading.RLock()
 BUTTONS_INSERTION_LOCK = threading.RLock()
+
 
 def add_note_to_db(chat_id, note_name, note_data, msgtype, buttons=None, file=None):
     if not buttons:
